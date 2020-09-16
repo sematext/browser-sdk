c18f3a8aeea24446212b5a1d335ebbef95dac636
Rafał Kuć <r.kuc@solr.pl>
2020-09-16 17:31:49 +0200

    Deployment script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+set -e
+
+BUCKET=$1
+CF_DISTRIBUTION=$2
+AWS_OPT=""
+
+echo "Deploying script to S3 bucket: $BUCKET using Cloudfront distribution ID: $CF_DISTRIBUTION" 
+
+HEAD=$(git rev-parse HEAD)
+cat > invalidation.json <<- EOM
+{
+  "Paths": {
+    "Quantity": 1,
+    "Items": ["/rum.js*"]
+  },
+  "CallerReference": "$HEAD"
+}
+EOM
+
+# Build script
+./build.sh
+
+aws s3 sync ./dist/ s3://$BUCKET $AWS_OPT
+
+aws cloudfront create-invalidation \
+    --invalidation-batch file://invalidation.json \
+    --distribution-id $CF_DISTRIBUTION
+
+echo 'Success!'
