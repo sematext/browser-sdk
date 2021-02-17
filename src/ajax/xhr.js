0fe4321698c5f2f576d34b03dcd0366bec51f710
Amir Hadzic <amir.hadzic@randomshouting.com>
2021-02-17 14:43:04 +0100

    Don't break XMLHttpRequest prototype

--- src/ajax/xhr.js
+++ src/ajax/xhr.js
@@ -4,32 +4,21 @@ import { taintUrl } from './common';
 
 export default function patchXhr(scopeObject: any) {
   const { XMLHttpRequest } = scopeObject;
-  scopeObject.XMLHttpRequest = () => {
-    const req = new XMLHttpRequest();
 
-    try {
-      const open = req.open.bind(req);
-      const send = req.send.bind(req);
-      let listeners;
+  const pOpen = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function open(method, originalUrl, ...args) {
+    const { url, taint } = taintUrl(originalUrl);
+    this.__listeners = createListeners(originalUrl);
+    this.addEventListener('loadend', (event) => {
+      this.__listeners.forEach(l => l.onLoadEnd && l.onLoadEnd(l, taint, event.target.status));
+    });
+    return pOpen.apply(this, [method, url, ...args]);
+  };
 
-      req.open = (method, originalUrl, ...props) => {
-        const { url, taint } = taintUrl(originalUrl);
-        listeners = createListeners(originalUrl);
-        req.addEventListener('loadend', (event) => {
-          listeners.forEach(l => l.onLoadEnd && l.onLoadEnd(l, taint, event.target.status));
-        });
 
-        return open.apply(req, [method, url, ...props]);
-      };
-
-      req.send = (...props) => {
-        listeners.forEach(l => l.onSend && l.onSend(l));
-        return send.apply(req, props);
-      };
-    } finally {
-      // do nothing
-    }
-
-    return req;
+  const pSend = XMLHttpRequest.prototype.send;
+  XMLHttpRequest.prototype.send = function send(...args) {
+    this.__listeners.forEach(l => l.onSend && l.onSend(l));
+    return pSend.apply(this, args);
   };
 }
